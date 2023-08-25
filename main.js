2203612487805f114a6d52364bc698768632706b
hasan  khaddour <125292041+HasanKhadd0ur@users.noreply.github.com>
2023-08-25 19:18:05 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,123 @@
+/*
+  function Break_Line(){
+    return  <hr style="width:100% ;border-style: dashed;margin-top: 60px;" /> ;
+  }
+
+  function Card(props){
+    return <card class={props.col}></card>
+  }
+  function Service(props){
+
+    return  <div class="row service" >{props.serviceTitle} </div>;
+          
+  }
+  function SeviceInfo(props){
+    return  <div class="row serviceTitle">
+              <b>{props.Res}</b>
+            </div>;        
+
+
+  }
+
+  function Section(props){
+    return <section class="row sction ">
+            <Service serviceTitle={props.serviceTitle}></Service>
+            <SeviceInfo Res={props.Res}></SeviceInfo>
+            
+            <Card col="col col-4"></Card>
+            <Card col="col col-4"></Card>
+            <Card col="col col-4"></Card>
+
+
+
+    </section>
+
+  }
+  const container = document.getElementById('root');
+  const root = ReactDOM.createRoot(container);
+  root.render(
+    <div>
+    
+    <Section serviceTitle="Our products " 
+             Res="Best co dsffdsdsf d df"
+             cardNumer="3"
+             colType="col-4_col-4"
+              > 
+    </Section>
+    
+    </div> 
+  )      
+
+
+*/
+  /*
+  
+     <hr style="width:100% ;border-style: dashed;margin-top: 60px;">
+         
+            <section class="row section">
+        
+                    <div class="row service" >Our services </div>
+                    <div class="row serviceTitle"><b>Best Consulting Solution</b></div>
+                    <div class="row">
+                        <card class="col col-4 "></card>
+                        <card class="col col-4 "></card>
+                        <card class="col col-4 "></card>
+                    </div>
+                    
+
+                </section>
+            <hr style="width:100% ;border-style: dashed;margin-top: 60px;">
+            <section class="row section">
+                
+                    <div class="row service" >Contact Us </div>
+                    <div class="row serviceTitle"><b>Call services Center for any Problem</b></div>
+                    <div class="row">
+                        
+                        <card class="col col-7 h700"></card>
+                        <card class="col col-5 h700"></card>
+                    </div>
+                    
+
+                    
+            </section>
+            <hr style="width:100% ;border-style: dashed;margin-top: 60px;">
+            
+            <section class="row section ">
+
+                   <div class="row service" >Our Customers </div>
+                    <div class="row serviceTitle"><b>Best Customers for year 2022</b></div>
+                    <div class="row">
+                        <card class="col col-4 "></card>
+                        <card class="col col-4 "></card>
+                        <card class="col col-4 "></card>
+                        <card class="col col-4 "></card>
+                        <card class="col col-4 "></card>
+                        <card class="col col-4 "></card>
+                    </div>
+                    
+                    
+
+                
+                    
+            </section>
+            <hr style="width:100% ;border-style: dashed;margin-top: 60px;">
+            <section class="row section ">
+
+                <div class="row service" >Our Products </div>
+                    <div class="row serviceTitle"><b>Best Choice for your  life</b></div>
+                    <div class="row">
+                        <card class="col col-2 "></card>
+                        <card class="col col-10 "></card>
+                        
+                    </div>
+                    
+                    
+
+                
+                    
+            </section>
+            <hr style="width:100% ;border-style: dashed;margin-top: 60px;">
+         
+  
+  
+  */
